7f8cb187eb64a097ea2f4332efbbadd5139d2572
Valentyn Krasniukevych <krasniukevich@gmail.com>
2022-10-31 14:00:07 +0200

    log envv

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { BlockService } from './block/block.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(parseInt(process.env.PORT) || 4000);
   console.log(process.env.NODE_ENV);
   console.log(process.env.MONGO_URL_PROD);
+  app.enableCors();
+  await app.listen(parseInt(process.env.PORT) || 4000);
   const blockService = app.get(BlockService);
   const lastBlockNumber = await blockService.getLastBlockNumber();
   blockService.fillDatabaseWithLast1000Blocks(lastBlockNumber);
